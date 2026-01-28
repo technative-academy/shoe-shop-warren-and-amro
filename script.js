1585b7434ce02c4965b82aac449661b5a6da453c
Amro <amro.youssef@ymail.com>
2026-01-28 16:33:07 +0000

    Fix tab functionality and add shoe images

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-class Tabs {
-  constructor(selector) {
-    this.tabsContainer = document.querySelector(selector);
-    this.tabButtons = this.tabsContainer.querySelectorAll(".tabs__button");
-    this.tabContents = this.tabsContainer.querySelectorAll(".tabs__content");
+const tabsContainer = document.querySelector(".tabs");
+const tabButtons = tabsContainer.querySelectorAll(".tabs__button");
+const tabContents = tabsContainer.querySelectorAll(".tabs__content");
 
-    this.tabButtons.forEach((tabButton) => {
-      tabButton.addEventListener("click", (event) => this.tabClicked(event));
-    });
-  }
+tabButtons.forEach((tabButton) => {
+    tabButton.addEventListener("click", (event) => tabClicked(event));
+});
 
-  tabClicked(event) {
+function tabClicked(event) {
     const clickedButton = event.currentTarget;
     const tabId = clickedButton.dataset.tab;
-    const detailsElement = this.tabsContainer.querySelector(
+    const detailsElement = tabsContainer.querySelector(
       `.tabs__content[data-tab-content="${tabId}"]`
     );
 
-    this.tabButtons.forEach((button) => {
+    tabButtons.forEach((button) => {
       button.classList.remove("tabs__button--selected");
     });
-    this.tabContents.forEach((content) => {
+    tabContents.forEach((content) => {
       content.classList.remove("tabs__content--selected");
     });
 
     clickedButton.classList.add("tabs__button--selected");
     detailsElement.classList.add("tabs__content--selected");
   }
-}
 
 // Expose the 'Tabs' class so that it can be imported by other files
-export default Tabs;
+// export default Tabs;
